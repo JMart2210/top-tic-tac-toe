0ea6f20957625933de8f74d647a1aacde269aef0
Jeremy Martin <46061444+JMart2210@users.noreply.github.com>
2023-01-28 09:07:51 -0500

    game works | minimal features | random cpu select

--- script.js
+++ script.js
@@ -1,11 +1,83 @@
 /* eslint-disable no-underscore-dangle */
 
+const Player = (name) => {
+  const side = 'X';
+  return { name, side };
+};
+
+const gamePlayModule = (function () {
+  const gameBoard = ['0', '', '', '', '', '', '', '', '', ''];
+  let xValues = [];
+  let oValues = [];
+  const player2 = false;
+  let winner = false;
+  const side = 'X';
+  let turn = 0;
+  const cpuChoice = null;
+  const winningCombos = [
+    [1, 2, 3],
+    [1, 4, 7],
+    [1, 5, 9],
+    [2, 5, 8],
+    [3, 5, 7],
+    [3, 6, 9],
+    [4, 5, 6],
+    [7, 8, 9]];
+  function gameOver(xOrO) {
+    console.log(xOrO);
+    document.querySelector(`.${xOrO}`).classList.add('visible');
+  }
+  function compareArray(testArray, ansArray) {
+    return ansArray.every((el) => testArray.includes(el));
+  }
+  function clickMove(space) {
+    const move = space.dataset.id;
+    updateGameBoard(move, side);
+    if (checkForWinner()) gameOver(side);
+    else {
+      if (player2) return;
+      if (++turn > 4) return;
+      updateGameBoard(getComputerChoice(), 'O');
+      if (checkForWinner()) gameOver('O');
+    }
+  }
+  function updateGameBoard(move, xOrO) {
+    gameBoard[move] = xOrO;
+    if (xOrO === 'X') xValues.push(+move);
+    else oValues.push(move);
+    gameBoardModule.updateDisplay(move, xOrO);
+  }
+  function getComputerChoice() {
+    let choice = '0';
+    const options = gameBoard.map((_, n) => n).filter((n) => gameBoard[n] === '');
+    while (choice === '0') {
+      choice = options[Math.floor(Math.random() * options.length)];
+    }
+    return choice;
+  }
+  function checkForWinner() {
+    console.log(xValues, oValues);
+    // const xValues = [];
+    // gameBoard.forEach((move, index) => (move === 'X' ? xValues.push(index) : null));
+    // const oValues = [];
+    // gameBoard.forEach((move, index) => (move === 'O' ? oValues.push(index) : null));
+
+    for (let i = 0; i < winningCombos.length; i++) {
+      // this goes through every winning combo and checks
+      // to see if every element is present
+      winner = compareArray(xValues, winningCombos[i]) || compareArray(oValues, winningCombos[i]);
+      if (winner) return true;
+    }
+  }
+  return { checkForWinner, clickMove, getComputerChoice };
+}());
+
 const gameBoardModule = (function () {
   function _createBoard() {
     // create container and buttons
     const container = document.querySelector('.gameContainer');
     const btn1 = document.createElement('button');
-    btn1.classList.add('gameBtn', 'top', 'left');
+    btn1.classList.add('gameBtn', 'btn1', 'top', 'left');
     btn1.dataset.id = '1';
     const btn2 = document.createElement('button');
     btn2.classList.add('gameBtn', 'btn2', 'top');
@@ -44,19 +116,21 @@ const gameBoardModule = (function () {
     row2.append(btn4, btn5, btn6);
     row3.append(btn7, btn8, btn9);
   }
-  function getBoard() {
+  function displayBoard() {
     _createBoard();
   }
-  function updateBoard(e) {
-    if (this.textContent) return;
-    this.textContent = 'X';
-    console.log(e.target.dataset.id);
+  function updateDisplay(space, side) {
+    document.querySelector(`.btn${space}`).textContent = side;
   }
-  return { getBoard, updateBoard };
+  return { displayBoard, updateDisplay };
 }());
 
-gameBoardModule.getBoard();
+gameBoardModule.displayBoard();
 
+// This kicks things off when player hits a square (game button)
 const gameBtns = document.querySelectorAll('.gameBtn');
-
-gameBtns.forEach((button) => button.addEventListener('click', gameBoardModule.updateBoard));
+gameBtns.forEach((button) => button.addEventListener('click', () => {
+  // this line quits function if space is already taken
+  if (button.textContent) return;
+  gamePlayModule.clickMove(button);
+}));
