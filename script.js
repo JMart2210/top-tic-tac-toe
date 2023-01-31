db5371f09f59c8b86df515534a3327f914dadacd
Jeremy Martin <46061444+JMart2210@users.noreply.github.com>
2023-01-31 15:22:54 -0500

    Added code comments to know what I was thinking

--- script.js
+++ script.js
@@ -22,17 +22,22 @@ const gamePlayModule = (function () {
     [4, 5, 6],
     [7, 8, 9]];
 
+  // This was the start of allowing user to decide X's or O's.
+  // but I never implemented that option.
   function switchMark() {
     clearBoard();
     [playerOne.mark, playerTwo.mark] = [playerTwo.mark, playerOne.mark];
   }
 
+  // compares an array with players moves to an array with winning
+  // moves. If present, returns the winning array.
   function compareArray(testArray, ansArray) {
     if (ansArray.every((el) => testArray.includes(el))) {
       return ansArray;
     }
   }
 
+  // Updates display to congratulate winner, or signify a tie
   function gameOver(winner) {
     DomElement.gameOverContainer.classList.add('visible');
     DomElement.mainDisplay.classList.add('blur');
@@ -41,33 +46,32 @@ const gamePlayModule = (function () {
       return;
     }
     DomElement.gameOverMsg.textContent = `${turn.name} wins!`;
+    // Winning class highlights the winning selections
     document.querySelector(`.btn${winner[0]}`).classList.add('winning');
     document.querySelector(`.btn${winner[1]}`).classList.add('winning');
     document.querySelector(`.btn${winner[2]}`).classList.add('winning');
   }
 
+  //swaps who's turn it is and increments turn number.
   function nextTurn() {
     [turn, otherTurn] = [otherTurn, turn];
-    // turn = (turn == playerOne) ? playerTwo : playerOne;
     turnNumber += 1;
   }
 
+  // receives move, updates game board and checks if they are a winner
   function clickAction(space) {
     const move = space.dataset.id;
     updateGameBoard(move, turn.mark);
     if (checkForWinner()) return gameOver(winner);
     nextTurn();
+    // we want to automatically get next move if the 
+    // game is not over and Player two is a cpu.
     if (playerTwo.name === 'cpu') {
       cpuChoice = getComputerChoice();
       updateGameBoard(cpuChoice, turn.mark);
       if (checkForWinner()) gameOver(winner);
       nextTurn();
     }
-    //   }
-    //   if (turnNumber > 8) return;
-    //   updateGameBoard(getComputerChoice(), 'O');
-    //   if (checkForWinner()) gameOver('O');
-    // }
   }
 
   // this updates the game board array as well as the
@@ -78,12 +82,13 @@ const gamePlayModule = (function () {
     gameBoardModule.updateDisplay(move, mark);
   }
 
+  // If a move played so far by the opponent matches a move needed
+  // in the winning combo, then that combo is removed. An array with 
+  // the remaining winning combos is returned.
   function getWinningCombos(movesSoFar) {
     const winningCombosLeft = [...winningCombos];
     for (let j = 0; j < movesSoFar.length; j++) {
       for (let i = winningCombosLeft.length - 1; i >= 0; i--) {
-      // this creates an object where the key is the move option,
-      // and the value is how many times it was present in the winning combos
         if (movesSoFar[j] === winningCombosLeft[i][0]
           || movesSoFar[j] === winningCombosLeft[i][1]
           || movesSoFar[j] === winningCombosLeft[i][2]) {
@@ -94,6 +99,10 @@ const gamePlayModule = (function () {
     return winningCombosLeft;
   }
 
+  // This returns where computer should play. It loops through the
+  // possible open moves, and returns if 1)you have a winning move,
+  // 2) your opponent has a "losing" move (so you can block it), or
+  // 3) the next best move based on the how useful it appears to be.
   function getComputerChoice() {
     const Board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     let moveOptions = {};
@@ -106,10 +115,6 @@ const gamePlayModule = (function () {
     openSquares = openSquares.filter((val) => !playerTwo.moves.includes(val));
     openSquares = openSquares.filter((val) => !playerOne.moves.includes(val));
 
-    // function loops through the possible open moves, and
-    // returns if 1)you have a winning move, 2) your opponent
-    // has a "losing" move (so you can block it), or 3) the next
-    // best move based on the how useful the moves appear to be.
     for (i = 0; i < winningCombos.length; i++) {
       for (j = 0; j < openSquares.length; j++) {
         if (turn === playerOne) {
@@ -136,18 +141,19 @@ const gamePlayModule = (function () {
     // the cpu to prevent the loss, instead of just making the winning move.
     if (theLosingMove) return theLosingMove;
 
-    // if (turn.mark == 'X' && )
+    // I could have simplified this now, since the turn variable is
+    // tied to the player, and it switches with every turn, but it works as-is,
+    // and it's fun to remember how I got things working.
     if (turn === playerOne) {
       opponentWinningMoves = getWinningCombos(playerOne.moves);
-      yourWinningMoves = getWinningCombos(playerTwo.moves);
     } else {
       opponentWinningMoves = getWinningCombos(playerTwo.moves);
-      yourWinningMoves = getWinningCombos(playerOne.moves);
     }
+    
+    // this loop creates an object that tells you which available space has the most
+    // appearances in the opponent's winning combos (aka which ones would be good to block)
     for (let i = 0; i < opponentWinningMoves.length; i++) {
       for (let j = 0; j < openSquares.length; j++) {
-      // this loop creates an object that tells you which available space has the most
-      // appearances in the opponent's winning combos (aka which ones would be good to block)
         if (openSquares[j] === opponentWinningMoves[i][0]) {
           moveOptions[opponentWinningMoves[i][0]] = (moveOptions[opponentWinningMoves[i][0]] + 1) || 1;
         }
@@ -160,10 +166,10 @@ const gamePlayModule = (function () {
       }
     }
 
+    // this loop adds to the same object above and tells you which available space
+    // has the most appearances in your winning combos (aka good to try and win)
     for (let i = 0; i < winningCombos.length; i++) {
       for (let j = 0; j < openSquares.length; j++) {
-      // this loop adds to the same object above and tells you which available space has the most
-      // appearances in your winning combos (aka good to try and win)
         if (openSquares[j] === winningCombos[i][0]) {
           moveOptions[winningCombos[i][0]] = (moveOptions[winningCombos[i][0]] + 1) || 1;
         }
@@ -176,15 +182,18 @@ const gamePlayModule = (function () {
       }
     }
     moveOptions = Object.entries(moveOptions).sort((a, b) => b[1] - a[1]).map((el) => el[0]).slice(0, 1);
+   
+    // cpuHelp function will only apply if no winning or losing
+    // move was found above.
     moveOptions = cpuHelper(moveOptions);
     return moveOptions;
   }
 
+  // instead of re-doing all of the cpu logic to figure out
+  // the failure at move 3, I just made a helper function
+  // that stops the blunder. Seems to work.
   function cpuHelper(move) {
     let help = move;
-    // instead of re-doing all of the cpu logic to figure out
-    // the failure at move 3, I just made a helper function
-    // that stops the blunder.
     if (turnNumber != 3) return help;
     if ((otherTurn.moves.includes(1) || otherTurn.moves.includes(3))
       && (otherTurn.moves.includes(8) || otherTurn.moves.includes(9))) help = 4;
@@ -192,12 +201,12 @@ const gamePlayModule = (function () {
       && (otherTurn.moves.includes(6) || otherTurn.moves.includes(3))) help = 8;
     return help;
   }
+
   function checkForWinner() {
     for (let i = 0; i < winningCombos.length; i++) {
       // this goes through every winning combo and checks
       // to see if every element is present
       winner = compareArray(turn.moves, winningCombos[i]);
-      // winner = compareArray(playerOne.moves, winningCombos[i]) || compareArray(playerTwo.moves, winningCombos[i]);
       if (winner) return winner;
       if (turnNumber === 8) {
         winner = 'Draw';
@@ -205,6 +214,8 @@ const gamePlayModule = (function () {
       }
     }
   }
+
+  // Resets gameboard and all pertinent fields
   function clearBoard() {
     gameBoard = ['0', '', '', '', '', '', '', '', '', ''];
     playerOne.moves = [];
@@ -220,6 +231,8 @@ const gamePlayModule = (function () {
     DomElement.gameOverContainer.classList.remove('visible');
     DomElement.mainDisplay.classList.remove('blur');
   }
+
+  // updates playerTwo to cpu.
   function cpuToggle() {
     clearBoard();
     if (playerTwo.name == 'cpu') {
@@ -282,12 +295,15 @@ const gameBoardModule = (function () {
     row2.append(btn4, btn5, btn6);
     row3.append(btn7, btn8, btn9);
   }
+
   function displayBoard() {
     _createBoard();
   }
+
   function updateDisplay(space, mark) {
     document.querySelector(`.btn${space}`).textContent = mark;
   }
+
   return { displayBoard, updateDisplay };
 }());
 
